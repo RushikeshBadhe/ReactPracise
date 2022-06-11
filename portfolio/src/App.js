837a636d82c0a25cbf5fd0ece9728fc8eeed337f
RushikeshBadhe <rushkeshb@cybage.com>
2022-06-11 19:23:04 +0530

    basic app created

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Intro from './componens/intro/Intro';
+
+function App() {
+  return (
+    <div>
+     <h2>Portfolio</h2>
+     <Intro/>
+    </div>
+  );
+}
+
+export default App;
