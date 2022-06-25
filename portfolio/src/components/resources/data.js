02262faf590055e0cd965cf38e6d3894f4e41fc0
RushikeshBadhe <rushkeshb@cybage.com>
2022-06-25 10:33:14 +0530

    links imgs added for resources

--- portfolio/src/components/resources/data.js
+++ portfolio/src/components/resources/data.js
@@ -1,8 +1,26 @@
 import reactPage from "./imgs/reactPage.JPG" 
+import mdnPage from "./imgs/mdnPage.JPG"
+import youtubePage from "./imgs/youtubePage.JPG";
+import w3Page from "./imgs/w3Page.JPG"
 export const data=[
     {
         id:1,
         img:reactPage,
         link:"https://reactjs.org/"
+    },
+    {
+        id:2,
+        img:mdnPage,
+        link:"https://mdn.dev/"
+    },
+    {
+        id:3,
+        img:youtubePage,
+        link:"https://www.youtube.com/results?search_query=react+js"
+    },
+    {
+        id:4,
+        img:w3Page,
+        link:"https://www.w3schools.com/"
     }
 ]
